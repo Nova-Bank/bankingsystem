3549c1ced3a79eff786abb36ad4d80f1325cd8c7
Toufic <touficcfares@gmail.com>
2025-11-06 13:01:48 -0500

    finance update

---
+++ src/model/Finance.java
@@ -0,0 +1,18 @@
+package model;
+public abstract class Finance {
+    private balance;
+    private total
+    void withdraw(double balance, double amount){
+        if(amount < 0){
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
+        balance -= amount;
+    }
+    void deposit(double balance, double amount){
+        if(amount < 0){
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
+        balance = balance - amount;
+    }
+
+}
