776b91d291b10317f79d160c055b1cbdde356ef7
touficcfares <touficcfares@gmail.com>
2025-11-14 19:50:18 -0500

    [feat] Credit card Done

--- src/main/java/com/github/novabank/model/Credit.java
+++ src/main/java/com/github/novabank/model/Credit.java
@@ -1,11 +1,14 @@
 package com.github.novabank.model;
 
+import java.time.ZoneId;
+
 public class Credit extends Finance{
     int creditBalance;
     int creditLimit;
-    int creditInterestRate;
+    double creditInterestRate = 0.21;
 
-    public Credit(int creditBalance, int creditLimit, int creditInterestRate) {
+    public Credit(int creditBalance, int creditLimit, double creditInterestRate) {
+        super(creditBalance, creditLimit, creditInterestRate);
         this.creditBalance = creditBalance;
         this.creditLimit = creditLimit;
         this.creditInterestRate = creditInterestRate;
@@ -19,7 +22,19 @@ public class Credit extends Finance{
      */
     @Override
     public void interest() {
+        int lastMonth = -1;
+        int m = java.time.LocalDate.now().getMonthValue();
 
+        if (lastMonth == -1) {
+            lastMonth = m;
+            return;
+        }
+
+        if(m != lastMonth && creditBalance > 0) {
+            creditBalance = (int) Math.round(creditBalance*(1+creditInterestRate));
+        }else{
+            creditBalance = 0;
+        }
     }
 
     public void purchase(int amount) {
