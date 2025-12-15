002532f01ae6a384be8734cc41ea36a2b1b072c3
Josef <josef50@my.yorku.ca>
2025-12-15 15:00:54 -0500

    [fix] Changed factory to return model objects

--- src/main/java/com/github/novabank/domain/finance/finance_creation/FinanceFactory.java
+++ src/main/java/com/github/novabank/domain/finance/finance_creation/FinanceFactory.java
@@ -1,53 +1,65 @@
 package com.github.novabank.domain.finance.finance_creation;
 
+import com.github.novabank.domain.finance.finance_accounts.Chequing;
+import com.github.novabank.domain.finance.finance_accounts.Credit;
+import com.github.novabank.domain.finance.finance_accounts.Savings;
+
 public class FinanceFactory {
 
-    public static ChequingBuilder createChequing(String type) {
+    public static Chequing createChequing(String type) {
         switch (type.toLowerCase()) {
             case "premium":
                 return new ChequingBuilder()
                         .setBalance(0)
                         .setDailyPurchaseLimit(3000000)
                         .setDailyWithdrawalLimit(999900)
-                        .setDailyTransferLimit(2000000);
+                        .setDailyTransferLimit(2000000)
+                        .build();
 
             case "base":
                 return new ChequingBuilder()
                         .setBalance(0)
                         .setDailyPurchaseLimit(500000)
                         .setDailyWithdrawalLimit(300000)
-                        .setDailyTransferLimit(300000);
+                        .setDailyTransferLimit(300000)
+                        .build();
 
             default:
                 throw new IllegalStateException("Unknown chequing type: " + type);
         }
     }
 
-    public static SavingsBuilder createSavings(String type) {
+    public static Savings createSavings(String type) {
         switch (type.toLowerCase()) {
             case "premium":
                 return new SavingsBuilder()
-                        .setInterestRate(0.03);
+                        .setInterestRate(0.03)
+                        .build();
 
             case "base":
                 return new SavingsBuilder()
-                        .setInterestRate(0.02);
+                        .setInterestRate(0.02)
+                        .build();
 
             default:
                 throw new IllegalStateException("Unknown savings type: " + type);
         }
     }
 
-    public static CreditBuilder createCredit(String type) {
+    public static Credit createCredit(String type) {
         switch (type.toLowerCase()) {
             case "black":
-                return new CreditBuilder().setCreditLimit(100000000);
+                return new CreditBuilder().setCreditLimit(100000000)
+                .build();
             case "red":
-                return new CreditBuilder().setCreditLimit(10000000);
+                return new CreditBuilder().setCreditLimit(10000000)
+                .build();
             case "silver":
-                return new CreditBuilder().setCreditLimit(1000000);
+                return new CreditBuilder().setCreditLimit(1000000)
+                .build();
             case "green":
-                return new CreditBuilder().setCreditLimit(300000);
+                return new CreditBuilder().setCreditLimit(300000)
+                .build();
             default:
                 throw new IllegalStateException("Unknown credit type: " + type);
         }
