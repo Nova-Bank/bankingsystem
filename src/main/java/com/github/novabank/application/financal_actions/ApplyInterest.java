a6d3de5ba85641eee257cb0401fc225d14de5e67
touficcfares <touficcfares@gmail.com>
2025-12-17 15:41:59 -0500

    [feat] Appplication layer done

--- src/main/java/com/github/novabank/application/financal_actions/ApplyInterest.java
+++ src/main/java/com/github/novabank/application/financal_actions/ApplyInterest.java
@@ -1,1 +1,22 @@
 package com.github.novabank.application.financal_actions;
+
+import com.github.novabank.domain.finance.FinanceRepository;
+import com.github.novabank.domain.finance.finance_accounts.Finance;
+import com.github.novabank.domain.finance.finance_creation.FinanceType;
+
+public class ApplyInterest {
+
+    private final FinanceRepository financeRepository;
+
+    public ApplyInterest(FinanceRepository financeRepository) {
+        this.financeRepository = financeRepository;
+    }
+
+    public void execute(String accountUid, FinanceType type) {
+        Finance finance = financeRepository.find(accountUid, type)
+                .orElseThrow(() -> new IllegalStateException("Finance not found for accountUid=" + accountUid + " type=" + type));
+
+        finance.interest();
+        financeRepository.save(accountUid, type, finance);
+    }
+}
