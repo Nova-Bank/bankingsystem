17982b50f3732aaf3fcda8cc0cf4e76f5af40903
touficcfares <touficcfares@gmail.com>
2025-12-18 19:50:23 -0500

    done Application

---
+++ src/main/java/com/github/novabank/presentation/NovaBankApplication.java
@@ -0,0 +1,13 @@
+package com.github.novabank.presentation;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaBankApplication.class, args);
+    }
+}
